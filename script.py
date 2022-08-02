24bd0e73902e50135b1af220a31749b90820ccaf
Ed Kubiak <ek33450505@gmail.com>
2022-08-01 21:40:57 -0400

    learning basic commands with Python

---
+++ script.py
@@ -0,0 +1,10 @@
+# The way to run a python file is like this on the command line: $>python script.py
+print("hello, world!")
+
+# indentation is important in python. The most common spacing to use is four, but must be at least one.
+if 5 > 2: 
+    print("five is greater than two!")
+
+# In Python, variables are created when you assign a value to it:
+x = 5
+y = "hello, world"
